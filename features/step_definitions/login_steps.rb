7880011feb23196b87fbf4f333f003119cd5281f
saikrishna <saikrishna321@yahoo.com>
2015-03-25 18:36:31 +0530

    pull test

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,19 +1,19 @@
 Given(/^I am  on the application login page$/) do
-
+ pending
 end
 
 When(/^i enter valid username as "(.*?)"$/) do |arg1|
-
+	pending
 end
 
 When(/^i enter valid password as "(.*?)"$/) do |arg1|
-
+	pending
 end
 
 When(/^i click on signin button$/) do
-
+	pending
 end
 
 Then(/^i must see the text "(.*?)"$/) do |arg1|
-
+	pending
 end
